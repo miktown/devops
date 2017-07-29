02f2f814dc82acf25630a01b60e603af90a60fc5
miktown <mcacho@mimotic.com>
2017-07-29 21:36:17 +0200

    added terraform and packer

---
+++ infra/main.tf
@@ -0,0 +1,50 @@
+resource "digitalocean_loadbalancer" "devops" {
+  name = "devops-html-v2"
+  region = "lon1"
+
+  forwarding_rule {
+    entry_port = 80
+    entry_protocol = "http"
+
+    target_port = 3000
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port = 3000
+    protocol = "http"
+  }
+
+  droplet_tag = "${digitalocean_tag.devops.name}"
+}
+
+resource "digitalocean_tag" "devops" {
+	name = "devops-html"
+}
+
+resource "digitalocean_droplet" "devops" {
+  count  = 3
+  image  = "26667153"
+  name   = "devops-v2"
+  region = "lon1"
+  size   = "512mb"
+  ssh_keys = [1692816]
+  tags   = ["${digitalocean_tag.devops.id}"]
+
+  user_data = <<EOF
+#cloud-config
+coreos:
+  units:
+    - name: "devops.service"
+      command: "start"
+      content: |
+        [Unit]
+        Description=Devops Demo
+        After=docker.service
+
+        [Service]
+        ExecStart=/usr/bin/docker run -d -p 3000:3000 devops
+EOF
+}
+
+
